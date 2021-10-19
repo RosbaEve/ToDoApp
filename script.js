71194dc3e18851885023c31a572ab13d6ad25721
Roosa Turunen <roosa.eveliina.turunen@gmail.com>
2021-10-19 21:51:02 +0300

    todo function

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const list = document.getElementById("list");       //getting elements
 const input = document.getElementById("input");
+let todoList = [];                                  // Creating empty array for todolistobjects
 
+function addToDo(ToDO){                             //function for adding todo item to list with cirlce icon for complete and trash for deleting
+    const ToDO = input.value;                       
+
+    const text = `<li class="item>
+                    <i class = " co fa fa-circle-thin" job="complete"></i>   
+                    <p class = "text">${ToDo}<p> 
+                    <i class="de fa fa-trash-o" job = "delete></i>`;
+
+    const position = "afterbegin";                  //Adding the new todo item to the top of the list
+
+    list.insertAdjacentHTML(position, text);        
+}
