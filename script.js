568ea91275d51a365a3a216bf21e9e865ebe9d78
Roosa Turunen <roosa.eveliina.turunen@gmail.com>
2021-10-20 10:28:33 +0300

    different icons for completed and uncompleted task

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const list = document.getElementById("list");       //getting elements
 const input = document.getElementById("input");
 let todoList = [];                                  // Creating empty array for todolistobjects
-let id = 0;
+let id = 0;                                         // setting id to zero by default
+
+const complete = "fa-check-circle-o";
+const uncomplete = "fa-circle-thin";
+let taskDone = document.getElementsByClassName("fa-cicrle-thin") //element and classes for checking if the task is complete
 
 function addToDo(toDo, id, done, trash){            //function for adding todo item to list withid & cirlce icon for complete and trash for deleting                    
     
     if (trash == true) {                            //If the element of trash is true, rest of the code will not be executed
         return;
     }
+    if (done == true) {                             //If done property is true, display check circle
+        taskDone = complete;
+    }
+    if (done == false){                             //If done property is false, display empty circle
+        taskDone = uncomplete;
+    }
 
     const text = `<li class = "item">
-                    <i class = "fa fa-circle-thin co" job= "complete" id = ${id}></i>
+                    <i class = "fa ${taskDone} co" job= "complete" id = ${id}></i>
                     <p class = "text">${toDo}</p> 
                     <i class=" fa fa-trash de" job = "delete" id = ${id}></i>
                  </li>`;
@@ -39,6 +49,8 @@ document.getElementById("submit-button").addEventListener("click", function(){ /
     }
     input.value = '';                               //Clearing the input field
 });
+
+addToDo("coffee", 1, true, false)
 
 list.addEventListener('click', function(event){     //Event listener for the list so that we can perform different functions based on what element inside of the list is clicked
     const element = event.target;
