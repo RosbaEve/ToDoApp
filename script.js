8d3a222eca3fb111e19528434d075789453b8a1c
Roosa Turunen <roosa.eveliina.turunen@gmail.com>
2021-10-20 09:19:25 +0300

    adding new todo with submit button

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ const list = document.getElementById("list");       //getting elements
 const input = document.getElementById("input");
 let todoList = [];                                  // Creating empty array for todolistobjects
 
-function addToDo(ToDO){                             //function for adding todo item to list with cirlce icon for complete and trash for deleting
-    const ToDO = input.value;                       
+function addToDo(toDo){                             //function for adding todo item to list with cirlce icon for complete and trash for deleting                    
 
-    const text = `<li class="item>
-                    <i class = " co fa fa-circle-thin" job="complete"></i>   
-                    <p class = "text">${ToDo}<p> 
-                    <i class="de fa fa-trash-o" job = "delete></i>`;
+    const text = `<li class = "item">
+                    <i class = "fa fa-circle-thin co" job="complete"></i>
+                    <p class = "text">${toDo}</p> 
+                    <i class=" fa fa-trash de" job = "delete"></i>
+                 </li>`;
 
     const position = "afterbegin";                  //Adding the new todo item to the top of the list
 
-    list.insertAdjacentHTML(position, text);        
+    list.insertAdjacentHTML(position, text);       
 }
+
+document.getElementById("submit-button").addEventListener("click", function(){ //When submit-button is clicked, function is performed
+    const toDo = input.value;                       //Assigning value for the toDo to be the text from input field
+
+    if (toDo !== '') {                              //Checking that the input is not empty
+        addToDo(toDo)
+    }
+    if (toDo == '') {                               //If input field is empty, the page will give alert to add a task to the field
+        alert("Please add task");
+    }
+    input.value = '';                               //Clearing the input field
+});
