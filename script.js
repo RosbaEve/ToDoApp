002757d9f1f001f14985f36d64cf25cf29860f4d
Roosa Turunen <roosa.eveliina.turunen@gmail.com>
2021-10-21 09:18:49 +0300

    counter for tasks left to do

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//elements and variables
+
 const list = document.getElementById("list");       //getting elements
 const input = document.getElementById("input");
 let todoList = [];                                  // Creating empty array for todolistobjects
@@ -9,6 +11,9 @@ let taskDone = document.getElementsByClassName("fa-cicrle-thin") //element and c
 
 let todoData = localStorage.getItem("task")         //Getting data from localstorage
 let clearButton = document.getElementById("clear")
+let count = 0;
+
+//Loading data from local storage
 
 if (todoData){                              //If there is data, perform loadlist function and set id for new task based on the lenghth of the list
     todoList = JSON.parse(todoData)
@@ -25,6 +30,8 @@ function loadlist(array){                          //Function for loading the li
     });
 }
 
+//Function addTodo
+
 function addToDo(toDo, id, done, trash){            //function for adding todo item to list withid & cirlce icon for complete and trash for deleting                    
     
     if (trash == true) {                            //If the element of trash is true, rest of the code will not be executed
@@ -32,6 +39,7 @@ function addToDo(toDo, id, done, trash){            //function for adding todo i
     }
     if (done == true) {                             //If done property is true, display check circle
         taskDone = complete;
+
     }
     if (done == false){                             //If done property is false, display empty circle
         taskDone = uncomplete;
@@ -47,6 +55,9 @@ function addToDo(toDo, id, done, trash){            //function for adding todo i
 
     list.insertAdjacentHTML(position, text);       
 }
+
+//Add Todo task with pressing enter
+
 document.addEventListener("keyup",function(event) { //Possibility to add task by pressing enter
     if (event.keyCode === 13){                      //Enter button keycode is 13 
         event.preventDefault();
@@ -54,6 +65,8 @@ document.addEventListener("keyup",function(event) { //Possibility to add task by
         document.getElementById("submit-button").click();   //Trigger submitkeys click with enter
     }
 });
+
+//Add todo by clicking submit button
 
 document.getElementById("submit-button").addEventListener("click", function(){ //When submit-button is clicked, function is performed
     const toDo = input.value;                       //Assigning value for the toDo to be the text from input field
@@ -75,6 +88,8 @@ document.getElementById("submit-button").addEventListener("click", function(){ /
         inputField.style.borderColor = "grey";
 
         id++;                                       //Id number increases so that every list object have unique id assigned 
+        count++;
+        document.getElementById('counter').innerHTML = count;
     }
     if (toDo == '') {                               //If input field is empty, the page will give alert to add a task to the field
         document.getElementById('feedback').innerHTML= "<br><b>Please add task<b>";
@@ -85,6 +100,8 @@ document.getElementById("submit-button").addEventListener("click", function(){ /
     input.value = '';                               //Clearing the input field
 });
 
+//Eventlisteners for complete and delete icons
+
 list.addEventListener('click', function(event){     //Event listener for the list so that we can perform different functions based on what element inside of the list is clicked
     const element = event.target;
     const elementJob = element.attributes.job.value;
@@ -94,25 +111,33 @@ list.addEventListener('click', function(event){     //Event listener for the lis
     }
     if (elementJob == "complete") {                 //If the job attribute is complete, perform complete function
         completeToDo(element);
+        moveElement(element)
     }
     localStorage.setItem("task",JSON.stringify(todoList));
 });
+
+//Delete function
 
 function deleteToDo(element){                       //Deleting the element from page and assigning trash value for true
     element.parentNode.parentNode.removeChild(element.parentNode);
     todoList[element.id].trash = true;
 }
 
+//Complete function
+
 function completeToDo(element){
 
     element.classList.toggle(uncomplete); 
-
+    
     if (todoList[element.id].done = true){
       element.classList.toggle(complete);
-      element.parentNode.style.opacity = "0.5";  
-    }
-
+      element.parentNode.style.opacity = "0.5";                //Change element opacity when it is marked done
+      count = count -1;                                        //Reduce the amount of tasks left by one
+      document.getElementById('counter').innerHTML = count;    //Display the new amount in counter
+    } 
 }
+
+//Clear local storage
 
 clearButton.addEventListener('click', function(){
     localStorage.clear();
